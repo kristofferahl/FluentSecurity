2c0bfbd682cd00b715f0e5e1fe77f8af8df3b8f2
kristofferahl <mail@77dynamite.com>
2011-01-15 20:58:28 +0000

    Changed to ASP.NET MVC 3 RTM Updated NUnit Updated build scripts

--- Click to build.bat
+++ Click to build.bat
@@ -1,2 +1,2 @@
-"C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe" "Build\Scripts\Build.msbuild" /property:Configuration=Build
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "Build\Scripts\Build.msbuild" /property:Configuration=Build
 PAUSE
