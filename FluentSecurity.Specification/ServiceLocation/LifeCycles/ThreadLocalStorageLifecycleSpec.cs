bd06a0ac35d83fcf6fc3139767c234147edff5f4
Kristoffer Ahl <git@77dynamite.com>
2012-03-10 22:26:56 +0100

    - Added support for 3 new lifecycles (HttpContext, ThreadLocalStorage and HybridHttp). - Added HttpContextLifecycle and specs. - Added HybridHttpContextLifecycle and specs. - Added ThreadLocalStorageLifecycle and specs.

---
+++ FluentSecurity.Specification/ServiceLocation/LifeCycles/ThreadLocalStorageLifecycleSpec.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using FluentSecurity.ServiceLocation.LifeCycles;
+using NUnit.Framework;
+
+namespace FluentSecurity.Specification.ServiceLocation.LifeCycles
+{
+	[TestFixture]
+	[Category("ThreadLocalStorageLifecycleSpec")]
+	public class When_getting_the_cache_from_ThreadLocalStorageLifecycle
+	{
+		[Test]
+		public void Should_get_same_cache_while_on_the_same_thread()
+		{
+			// Act
+			var cache1 = new ThreadLocalStorageLifecycle().FindCache();
+			var cache2 = new ThreadLocalStorageLifecycle().FindCache();
+
+			// Assert
+			Assert.That(cache1, Is.EqualTo(cache2));
+		}
+
+		[Test]
+		public void Should_get_different_caches_for_2_threads()
+		{
+			// Arrange & act
+			var task1 = new Task<IObjectCache>(() => new ThreadLocalStorageLifecycle().FindCache());
+			task1.Start();
+
+			var task2 = new Task<IObjectCache>(() => new ThreadLocalStorageLifecycle().FindCache());
+			task2.Start();
+
+			Task.WaitAll(task1, task2);
+
+			var cache1 = task1.Result;
+			var cache2 = task2.Result;
+
+			// Assert
+			Assert.That(cache1, Is.Not.EqualTo(cache2));
+		}
+	}
+}
