85630c4bd700a3f657dc51ca59e3535936091ced
Kristoffer Ahl <git@77dynamite.com>
2012-11-22 13:52:27 +0100

    - Made the SecurityModel implement the IAdvanced interface. - Added specs for SecurityModel. - Made SecurityModel internal.

---
+++ FluentSecurity.Specification/SecurityModelSpec.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using FluentSecurity.Caching;
+using NUnit.Framework;
+
+namespace FluentSecurity.Specification
+{
+	[TestFixture]
+	[Category("SecurityModelSpec")]
+	public class When_creating_a_new_security_model
+	{
+		private SecurityModel _securityModel;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_securityModel = new SecurityModel();
+		}
+
+		[Test]
+		public void Should_not_have_any_policy_containers()
+		{
+			Assert.That(_securityModel.Profiles.Count(), Is.EqualTo(0));
+		}
+
+		[Test]
+		public void Should_not_have_any_profiles()
+		{
+			Assert.That(_securityModel.Profiles.Count(), Is.EqualTo(0));
+		}
+
+		[Test]
+		public void Should_not_have_any_conventions()
+		{
+			Assert.That(_securityModel.Conventions.Count(), Is.EqualTo(0));
+		}
+
+		[Test]
+		public void Should_not_ignore_missing_configurations()
+		{
+			Assert.That(_securityModel.ShouldIgnoreMissingConfiguration, Is.False);
+		}
+
+		[Test]
+		public void Should_have_default_policy_cache_lifecycle_set_to_DoNotCache()
+		{
+			Assert.That(_securityModel.DefaultResultsCacheLifecycle, Is.EqualTo(Cache.DoNotCache));
+		}
+
+		[Test]
+		public void Should_not_have_a_security_context_modifyer()
+		{
+			Assert.That(_securityModel.SecurityContextModifyer, Is.Null);
+		}
+	}
+}
--- FluentSecurity/SecurityModel.cs
+++ FluentSecurity/SecurityModel.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using FluentSecurity.Caching;
+using FluentSecurity.Internals;
 
 namespace FluentSecurity
 {
-	public class SecurityModel
+	internal class SecurityModel : IAdvanced
 	{
-		internal IList<Type> Profiles { get; private set; }
-		internal IList<IPolicyContainer> PolicyContainers { get; private set; } 
+		public IList<Type> Profiles { get; private set; }
+		public IList<IPolicyContainer> PolicyContainers { get; private set; }
+		public Conventions Conventions { get; private set; }
+		
+		public Cache DefaultResultsCacheLifecycle { get; internal set; }
+		public Action<ISecurityContext> SecurityContextModifyer { get; internal set; }
+		public bool ShouldIgnoreMissingConfiguration { get; internal set; }
 
 		public SecurityModel()
 		{
 			Profiles = new List<Type>();
 			PolicyContainers = new List<IPolicyContainer>();
+			Conventions = new Conventions();
+			ShouldIgnoreMissingConfiguration = false;
+			DefaultResultsCacheLifecycle = Cache.DoNotCache;
 		}
 	}
 }
