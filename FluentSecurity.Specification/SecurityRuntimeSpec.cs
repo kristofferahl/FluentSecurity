d417ddbce6fcf014c27b834cfa4c838321fb47e8
Kristoffer Ahl <git@77dynamite.com>
2012-12-04 19:03:57 +0100

    - Added specs for null checks in SecurityRuntime.

--- FluentSecurity.Specification/SecurityRuntimeSpec.cs
+++ FluentSecurity.Specification/SecurityRuntimeSpec.cs
@@ -1,48 +1,105 @@
-﻿using FluentSecurity.Caching;
+﻿using System;
+using FluentSecurity.Caching;
+using FluentSecurity.Configuration;
 using NUnit.Framework;
 
 namespace FluentSecurity.Specification
 {
-	[TestFixture]
-	[Category("SecurityRuntimeSpec")]
-	public class When_creating_a_SecurityRuntime
+	public class When_creating_a_SecurityRuntime : SecurityRuntimeSpecification
 	{
-		private SecurityRuntime _runtime;
-
-		[SetUp]
-		public void SetUp()
-		{
-			_runtime = new SecurityRuntime();
-		}
-
 		[Test]
 		public void Should_not_ignore_missing_configuration()
 		{
-			Assert.That(_runtime.ShouldIgnoreMissingConfiguration, Is.False);
+			Assert.That(Runtime.ShouldIgnoreMissingConfiguration, Is.False);
 		}
 
 		[Test]
 		public void Should_not_cache_results_by_default()
 		{
-			Assert.That(_runtime.DefaultResultsCacheLifecycle, Is.EqualTo(Cache.DoNotCache));
+			Assert.That(Runtime.DefaultResultsCacheLifecycle, Is.EqualTo(Cache.DoNotCache));
 		}
 
 		[Test]
 		public void Should_not_have_any_profiles()
 		{
-			Assert.That(_runtime.Profiles, Is.Empty);
+			Assert.That(Runtime.Profiles, Is.Empty);
 		}
 
 		[Test]
 		public void Should_not_have_any_policy_containers()
 		{
-			Assert.That(_runtime.PolicyContainers, Is.Empty);
+			Assert.That(Runtime.PolicyContainers, Is.Empty);
 		}
 
 		[Test]
 		public void Should_not_have_any_conventions()
 		{
-			Assert.That(_runtime.Conventions, Is.Empty);
+			Assert.That(Runtime.Conventions, Is.Empty);
+		}
+	}
+
+	public class When_applying_ConventionConfiguration_to_SecurityRuntime : SecurityRuntimeSpecification
+	{
+		[Test]
+		public void Should_throw_when_null()
+		{
+			// Arrange
+			Action<ConventionConfiguration> configuration = null;
+
+			// Act & Assert
+			Assert.Throws<ArgumentNullException>(() => Runtime.ApplyConfiguration(configuration));
+		}
+	}
+
+	public class When_applying_ViolationConfiguration_to_SecurityRuntime : SecurityRuntimeSpecification
+	{
+		[Test]
+		public void Should_throw_when_null()
+		{
+			// Arrange
+			Action<ViolationConfiguration> configuration = null;
+
+			// Act & Assert
+			Assert.Throws<ArgumentNullException>(() => Runtime.ApplyConfiguration(configuration));
+		}
+	}
+
+	public class When_applying_a_SecurityProfile_to_SecurityRuntime : SecurityRuntimeSpecification
+	{
+		[Test]
+		public void Should_throw_when_null()
+		{
+			// Arrange
+			SecurityProfile configuration = null;
+
+			// Act & Assert
+			Assert.Throws<ArgumentNullException>(() => Runtime.ApplyConfiguration(configuration));
+		}
+	}
+
+	public class When_adding_a_PolicyContainer_to_SecurityRuntime : SecurityRuntimeSpecification
+	{
+		[Test]
+		public void Should_throw_when_null()
+		{
+			// Arrange
+			PolicyContainer policyContainer = null;
+
+			// Act & Assert
+			Assert.Throws<ArgumentNullException>(() => Runtime.AddPolicyContainer(policyContainer));
+		}
+	}
+
+	[TestFixture]
+	[Category("SecurityRuntimeSpec")]
+	public abstract class SecurityRuntimeSpecification
+	{
+		internal SecurityRuntime Runtime;
+
+		[SetUp]
+		public void SetUp()
+		{
+			Runtime = new SecurityRuntime();
 		}
 	}
 }
