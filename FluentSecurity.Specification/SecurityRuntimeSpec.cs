4f4148f8c1feff0e7cd79b2730a42ae9e12c4a1c
Kristoffer Ahl <git@77dynamite.com>
2012-12-04 18:54:22 +0100

    - Added specs for defaults of SecurityRuntime.

---
+++ FluentSecurity.Specification/SecurityRuntimeSpec.cs
@@ -0,0 +1,48 @@
+﻿using FluentSecurity.Caching;
+using NUnit.Framework;
+
+namespace FluentSecurity.Specification
+{
+	[TestFixture]
+	[Category("SecurityRuntimeSpec")]
+	public class When_creating_a_SecurityRuntime
+	{
+		private SecurityRuntime _runtime;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_runtime = new SecurityRuntime();
+		}
+
+		[Test]
+		public void Should_not_ignore_missing_configuration()
+		{
+			Assert.That(_runtime.ShouldIgnoreMissingConfiguration, Is.False);
+		}
+
+		[Test]
+		public void Should_not_cache_results_by_default()
+		{
+			Assert.That(_runtime.DefaultResultsCacheLifecycle, Is.EqualTo(Cache.DoNotCache));
+		}
+
+		[Test]
+		public void Should_not_have_any_profiles()
+		{
+			Assert.That(_runtime.Profiles, Is.Empty);
+		}
+
+		[Test]
+		public void Should_not_have_any_policy_containers()
+		{
+			Assert.That(_runtime.PolicyContainers, Is.Empty);
+		}
+
+		[Test]
+		public void Should_not_have_any_conventions()
+		{
+			Assert.That(_runtime.Conventions, Is.Empty);
+		}
+	}
+}
