0e0a361540f838df5b73fe1fa2a24a30337f5ab2
Kristoffer Ahl <git@77dynamite.com>
2013-02-15 20:24:44 +0100

    - Updated the sample application to showcase how you can track events occuring in FluentSecurity.

--- SampleApplication/FluentSecurity.SampleApplication/Bootstrapper.cs
+++ SampleApplication/FluentSecurity.SampleApplication/Bootstrapper.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using System.Web;
+using FluentSecurity.Diagnostics;
+using FluentSecurity.Diagnostics.Events;
 using FluentSecurity.SampleApplication.Areas.ExampleArea;
 using FluentSecurity.SampleApplication.Controllers;
 using FluentSecurity.SampleApplication.Models;
@@ -7,6 +10,8 @@ namespace FluentSecurity.SampleApplication
 {
 	public static class Bootstrapper
 	{
+		public static List<ISecurityEvent> Events = new List<ISecurityEvent>();
+		
 		public static ISecurityConfiguration SetupFluentSecurity()
 		{
 			SecurityConfigurator.Configure(configuration =>
@@ -43,6 +48,8 @@ namespace FluentSecurity.SampleApplication
 
 				configuration.ApplyProfile<CrudControllerProfile>();
 			});
+			
+			EventListeners.Register(Events.Add);
 			return SecurityConfiguration.Current;
 		}
 	}
