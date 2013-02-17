8e6aa36bc9f27ef4cc83f03863d0a7900bfac998
Kristoffer Ahl <git@77dynamite.com>
2013-02-17 23:05:22 +0100

    - Added ISecurityEventListener interface and implementation AnonymousSecurityEventListener.

---
+++ FluentSecurity/Diagnostics/AnonymousSecurityEventListener.cs
@@ -0,0 +1,24 @@
+using System;
+using FluentSecurity.Diagnostics.Events;
+
+namespace FluentSecurity.Diagnostics
+{
+	public class AnonymousSecurityEventListener : ISecurityEventListener
+	{
+		public Action<ISecurityEvent> EventListener { get; private set; }
+
+		public AnonymousSecurityEventListener(Action<ISecurityEvent> eventListener)
+		{
+			EventListener = eventListener;
+		}
+
+		public void Handle(ISecurityEvent securityEvent)
+		{
+			try
+			{
+				EventListener.Invoke(securityEvent);
+			}
+			catch {}
+		}
+	}
+}
