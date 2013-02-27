8da1583ec188cd83ee513750fc4ad2fb14a3745d
Kristoffer Ahl <git@77dynamite.com>
2013-02-27 22:15:06 +0100

    - Replaced NAnt build script with Koshu build script. - Removed old trigger files (Build.bat, Pack.bat, Release,bat). - Removed NAnt and the tools directory.

---
+++ build.ps1
@@ -0,0 +1,178 @@
+properties {
+	$product		= 'FluentSecurity'
+	$version		= '2.0.0'
+	$label			= 'alpha4'
+	$configuration	= 'release'
+	$useVerbose		= $false
+
+	$rootDir		= '.'
+	$sourceDir		= "$rootDir"
+	$buildDir		= "$rootDir\Build"
+	$outputDir		= "$buildDir\Output"
+	$reportsDir		= "$buildDir\Reports"
+	$artifactsDir	= "$buildDir\Artifacts"	
+	$artifactsName	= "$product-$version-$configuration" -replace "\.","_"
+	
+	$buildNumber	= $null
+	
+	$copyright		= 'Copyright (c) 2009-2013, Kristoffer Ahl'
+	
+	$setupMessage	= 'Executed Setup!'
+	$cleanMessage	= 'Executed Clean!'
+	$compileMessage	= 'Executed Compile!'
+	$testMessage	= 'Executed Test!'
+	$packMessage	= 'Executed Pack!'
+	$deployMessage	= 'Executed Deploy!'
+}
+
+task default -depends Local
+
+task Local {
+	Write-Host "Running local build" -fore Yellow
+	Write-Host "Product:        $product" -fore Yellow
+	Write-Host "Version:        $version" -fore Yellow
+	Write-Host "Build version:  $buildVersion" -fore Yellow
+	Write-Host "Build label:    $buildLabel" -fore Yellow
+	Invoke-Task Deploy
+}
+task Release {
+	Write-Host "Running release build" -fore Yellow
+	Write-Host "Product:        $product" -fore Yellow
+	Write-Host "Version:        $version" -fore Yellow
+	Write-Host "Build version:  $buildVersion" -fore Yellow
+	Write-Host "Build label:    $buildLabel" -fore Yellow
+	Invoke-Task Deploy
+}
+
+task Setup {
+	nuget install ".nuget\packages.config" -outputdirectory "packages"
+	generate_assemblyinfo `
+		-file "$sourceDir\SharedAssemblyInfo.cs" `
+		-description "$product ($configuration)" `
+		-company $company `
+		-product $product `
+		-version $version `
+		-buildVersion $buildVersion `
+		-buildLabel $buildLabel `
+		-clsCompliant "false" `
+		-copyright $copyright
+	$setupMessage
+}
+
+task Clean {
+	delete_directory $artifactsDir
+	delete_directory $outputDir
+	delete_directory $reportsDir
+	create_directory $artifactsDir
+	create_directory $outputDir
+	create_directory $reportsDir
+	$cleanMessage
+}
+
+task Compile -depends Setup, Clean {
+	build_solution "$sourceDir\$product.sln"
+	$compileMessage
+}
+
+task Test -depends Compile {
+	run_tests $outputDir "*.Specification.dll"
+	$testMessage
+}
+
+task Pack -depends Test {
+	create_directory "$artifactsDir\$artifactsName"
+	
+	$fluentDir = (join-path -path (resolve-path "$artifactsDir\$artifactsName") -childpath "\FluentSecurity")
+	
+	create_directory $fluentDir
+	
+	copy-item "$outputDir\FluentSecurity.dll" $fluentDir
+	copy-item "$outputDir\FluentSecurity.TestHelper.dll" $fluentDir
+	copy-item "$rootDir\License.txt" $fluentDir
+	copy-item "$rootDir\Readme.md" $fluentDir
+	
+	get-content "$buildDir\NuGet\FluentSecurity.nuspec" | % { $_ -replace "@CURRENT-VERSION@", $buildLabel -replace "@ARTIFACT-PATH@", $fluentDir } | set-content "$buildDir\FluentSecurity.nuspec"
+	get-content "$buildDir\NuGet\FluentSecurity.TestHelper.nuspec" | % { $_ -replace "@CURRENT-VERSION@", $buildLabel -replace "@ARTIFACT-PATH@", $fluentDir } | set-content "$buildDir\FluentSecurity.TestHelper.nuspec"
+	
+	nuget pack "$buildDir\FluentSecurity.nuspec" -OutputDirectory "$artifactsDir\$artifactsName"
+	nuget pack "$buildDir\FluentSecurity.TestHelper.nuspec" -OutputDirectory "$artifactsDir\$artifactsName"
+	
+	$packMessage
+}
+
+task Deploy -depends Pack {
+	$localFeedDir = "C:\Develop\LocalNugetFeed"
+	if (test-path $localFeedDir) {
+		copy_files "$artifactsDir\$artifactsName" $localFeedDir "*.nupkg"
+	}
+	$deployMessage
+}
+
+task ? -Description "Help" {
+	Write-Documentation
+}
+
+taskSetup {
+	$script:buildVersion	= ?: {$buildNumber -ne $null} {"$version.$buildNumber"} {$version}
+	$script:buildLabel		= ?: {$label -ne $null -and $label -ne ''} {"$buildVersion-$label"} {$buildVersion}
+}
+
+#------------------------------------------------------------
+# Reusable functions
+#------------------------------------------------------------
+
+function generate_assemblyinfo {
+	param(
+		[string]$clsCompliant = "true",
+		[string]$description, 
+		[string]$company, 
+		[string]$product, 
+		[string]$copyright, 
+		[string]$version,
+		[string]$buildVersion,
+		[string]$buildLabel,
+		[string]$file = $(throw "file is a required parameter.")
+	)
+	
+	$asmInfo = "using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliantAttribute($clsCompliant)]
+[assembly: ComVisibleAttribute(false)]
+[assembly: AssemblyDescriptionAttribute(""$description"")]
+[assembly: AssemblyCompanyAttribute(""$company"")]
+[assembly: AssemblyProductAttribute(""$product"")]
+[assembly: AssemblyTitleAttribute(""$product"")]
+[assembly: AssemblyCopyrightAttribute(""$copyright"")]
+[assembly: AssemblyVersionAttribute(""$version"")]
+[assembly: AssemblyInformationalVersionAttribute(""$buildLabel"")]
+[assembly: AssemblyFileVersionAttribute(""$buildVersion"")]"
+
+	$dir = [System.IO.Path]::GetDirectoryName($file)
+	if ([System.IO.Directory]::Exists($dir) -eq $false)
+	{
+		Write-Host "Creating directory $dir"
+		[System.IO.Directory]::CreateDirectory($dir)
+	}
+	
+	Write-Host "Generating assembly info file: $file"
+	out-file -filePath $file -encoding UTF8 -inputObject $asmInfo
+}
+
+function run_tests($source, $include=@("*.dll")) {
+	$testassemblies = get-childitem $source -recurse -include $include 
+	$testassemblies | foreach-object {
+		$assembly = $_
+		$assemblyName = $_.BaseName
+		write-host "Running test in $assembly."
+		exec {
+			& $sourceDir\packages\NUnit.Runners.2.6.2\tools\nunit-console-x86.exe $assembly /nologo /framework:net-4.5 /xml="$reportsDir\$assemblyName-tests-results.xml"
+		}
+	}
+}
+
+function read_xml($file) {
+	[xml]$xml = Get-Content $file
+	return $xml
+}
