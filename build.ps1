7dd3ad75f6ac7a3638369f3e8a98f7deb4a01e23
Kristoffer Ahl <git@77dynamite.com>
2013-04-15 13:08:59 +0200

    - Updated the feed url for the public nuget feed in build script. - Added switch for apikey for nuget push command in build script.

--- build.ps1
+++ build.ps1
@@ -135,14 +135,14 @@ task Deploy -depends Pack {
 	}
 	
 	if ($branch -eq 'master' -and $nugetApiKey -ne $null) {
-		$feed = 'https://nuget.org/api/v2/'
+		$feed = 'https://nuget.org/'
 		$apiKey = $nugetApiKey
 	}
 	
 	if ($feed -ne $null -and $apiKey -ne $null) {
 		get-childitem -path "$artifactsDir\$artifactsName" -filter "*.nupkg" | % {
 			write-host "Pushing nuget package $_ to $feed" -fore cyan
-			nuget_exe push $_.FullName $apiKey -source $feed
+			nuget_exe push $_.FullName -apikey $apiKey -source $feed
 		}
 	}
 	
