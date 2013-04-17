9337724c0915c91e4f691b800d000c3fbf062ab3
Kristoffer Ahl <git@77dynamite.com>
2013-04-17 16:23:47 +0200

    - Simplified the SecurityEventListenerScanner by using GetLoadableExportedTypes extension for Assembly.

--- FluentSecurity.Specification/Diagnostics/SecurityEventListenerScannerSpec.cs
+++ FluentSecurity.Specification/Diagnostics/SecurityEventListenerScannerSpec.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using FluentSecurity.Diagnostics;
@@ -15,7 +14,7 @@ namespace FluentSecurity.Specification.Diagnostics
 		public void Should_find_0_event_listeners()
 		{
 			// Arrange
-			var scanner = new SecurityEventListenerScanner(false);
+			var scanner = new SecurityEventListenerScanner();
 			var assemblies = new List<Assembly> { scanner.GetType().Assembly };
 
 			// Act
@@ -29,7 +28,7 @@ namespace FluentSecurity.Specification.Diagnostics
 		public void Should_find_1_event_listener()
 		{
 			// Arrange
-			var scanner = new SecurityEventListenerScanner(false);
+			var scanner = new SecurityEventListenerScanner();
 			var assemblies = new List<Assembly> { GetType().Assembly };
 
 			// Act
@@ -40,26 +39,10 @@ namespace FluentSecurity.Specification.Diagnostics
 		}
 
 		[Test]
-		public void Should_handle_TypeLoadException_and_find_1_event_listener()
+		public void Should_find_2_event_listeners()
 		{
 			// Arrange
-			var log = new List<string>();
-			SecurityDoctor.Register(e => log.Add(e.Message));
-			
-			var throwException = true;
-			const string expectedExceptionMessage = "Could not load type X.";
-
-			Func<Assembly, Type[]> assemblyTypeProvider = assembly =>
-			{
-				if (throwException)
-				{
-					throwException = false;
-					throw new TypeLoadException(expectedExceptionMessage);
-				}
-				return assembly.GetExportedTypes();
-			};
-
-			var scanner = new SecurityEventListenerScanner(true, assemblyTypeProvider);
+			var scanner = new SecurityEventListenerScanner();
 			var assemblies = new List<Assembly>
 			{
 				GetType().Assembly,
@@ -70,42 +53,7 @@ namespace FluentSecurity.Specification.Diagnostics
 			var types = scanner.Scan(assemblies);
 
 			// Assert
-			Assert.That(types.Count(), Is.EqualTo(1));
-			Assert.That(log.Single(), Is.EqualTo(expectedExceptionMessage));
-		}
-
-		[Test]
-		public void Should_throw_TypeLoadException()
-		{
-			// Arrange
-			var log = new List<string>();
-			SecurityDoctor.Register(e => log.Add(e.Message));
-	
-			var throwException = true;
-			const string expectedExceptionMessage = "Could not load type Y.";
-
-			Func<Assembly, Type[]> assemblyTypeProvider = assembly =>
-			{
-				if (throwException)
-				{
-					throwException = false;
-					throw new TypeLoadException(expectedExceptionMessage);
-				}
-				return assembly.GetExportedTypes();
-			};
-			
-			var scanner = new SecurityEventListenerScanner(false, assemblyTypeProvider);
-			var assemblies = new List<Assembly>
-			{
-				GetType().Assembly
-			};
-
-			// Act
-			var exception = Assert.Throws<TypeLoadException>(() => scanner.Scan(assemblies));
-
-			// Assert
-			Assert.That(exception.Message, Is.EqualTo(expectedExceptionMessage));
-			Assert.That(log.Single(), Is.EqualTo(expectedExceptionMessage));
+			Assert.That(types.Count(), Is.EqualTo(2));
 		}
 	}
 }
