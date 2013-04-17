56e9bb598b951a1eb8455c1292a3a5f7376b73b6
Kristoffer Ahl <git@77dynamite.com>
2013-04-17 16:22:56 +0200

    - Added GetLoadableTypes and GetLoadableExportedTypes extensions for Assembly. - Added and implemented specs for AssemblyExtensions.

---
+++ FluentSecurity.Specification/Internals/AssemblyExtensionsSpec.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using FluentSecurity.Specification.TestData;
+using FluentSecurity.Specification.TestData.Controllers;
+using NUnit.Framework;
+using FluentSecurity.Internals;
+
+namespace FluentSecurity.Specification.Internals
+{
+	[TestFixture]
+	[Category("AssemblyExtensionsSpec")]
+	public class When_getting_the_types_of_an_assembly
+	{
+		[SetUp]
+		public void SetUp()
+		{
+			AssemblyExtensions.Reset();
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			AssemblyExtensions.Reset();
+		}
+
+		[Test]
+		public void Should_throw_when_assembly_is_null()
+		{
+			// Arrange
+			Assembly assembly = null;
+
+			// Act
+			Assert.Throws<ArgumentNullException>(() => assembly.GetLoadableTypes());
+		}
+
+		[Test]
+		public void Should_return_types()
+		{
+			// Arrange
+			var assembly = GetType().Assembly;
+
+			// Act
+			var types = assembly.GetLoadableTypes();
+
+			// Assert
+			Assert.That(types.Any(), Is.True);
+		}
+
+		[Test]
+		public void Should_return_types_successfully_loaded()
+		{
+			// Arrange
+			AssemblyExtensions.GetTypesProvider = a =>
+			{
+				throw new ReflectionTypeLoadException(new List<Type>
+				{
+					typeof(AdminController),
+					null,
+					typeof(BlogController),
+					null,
+					null
+				}.ToArray(), new Exception[0]);
+			};
+			var assembly = GetType().Assembly;
+
+			// Act
+			var types = assembly.GetLoadableTypes();
+
+			// Assert
+			Assert.That(types.Count(), Is.EqualTo(2));
+		}
+	}
+
+	[TestFixture]
+	[Category("AssemblyExtensionsSpec")]
+	public class When_getting_the_exported_types_of_an_assembly
+	{
+		[SetUp]
+		public void SetUp()
+		{
+			AssemblyExtensions.Reset();
+		}
+
+		[Test]
+		public void Should_throw_when_assembly_is_null()
+		{
+			// Arrange
+			Assembly assembly = null;
+
+			// Act
+			Assert.Throws<ArgumentNullException>(() => assembly.GetLoadableExportedTypes());
+		}
+
+		[Test]
+		public void Should_return_types()
+		{
+			// Arrange
+			var assembly = GetType().Assembly;
+
+			// Act
+			var types = assembly.GetLoadableExportedTypes();
+
+			// Assert
+			Assert.That(types.Any(), Is.True);
+		}
+
+		[Test]
+		public void Should_return_types_successfully_loaded()
+		{
+			// Arrange
+			AssemblyExtensions.GetTypesProvider = a =>
+			{
+				throw new ReflectionTypeLoadException(new List<Type>
+				{
+					typeof(AdminController),
+					null,
+					typeof(BlogController),
+					null,
+					null
+				}.ToArray(), new Exception[0]);
+			};
+			var assembly = GetType().Assembly;
+
+			// Act
+			var types = assembly.GetLoadableExportedTypes();
+
+			// Assert
+			Assert.That(types.Count(), Is.EqualTo(2));
+		}
+	}
+}
---
+++ FluentSecurity/Internals/AssemblyExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace FluentSecurity.Internals
+{
+	internal static class AssemblyExtensions
+	{
+		internal static Func<Assembly, IEnumerable<Type>> GetTypesProvider;
+
+		static AssemblyExtensions()
+		{
+			Reset();
+		}
+
+		internal static void Reset()
+		{
+			GetTypesProvider = assembly => assembly.GetTypes();
+		}
+
+		internal static IEnumerable<Type> GetLoadableTypes(this Assembly assembly)
+		{
+			if (assembly == null) throw new ArgumentNullException("assembly");
+			try
+			{
+				return GetTypesProvider.Invoke(assembly).Where(type => type.IsVisible);
+			}
+			catch (ReflectionTypeLoadException e)
+			{
+				return e.Types.Where(type => type != null);
+			}
+		}
+
+		internal static IEnumerable<Type> GetLoadableExportedTypes(this Assembly assembly)
+		{
+			if (assembly == null) throw new ArgumentNullException("assembly");
+			try
+			{
+				return GetTypesProvider.Invoke(assembly).Where(type => type.IsVisible);
+			}
+			catch (ReflectionTypeLoadException e)
+			{
+				return e.Types.Where(type => type != null && type.IsVisible);
+			}
+		} 
+	}
+}
