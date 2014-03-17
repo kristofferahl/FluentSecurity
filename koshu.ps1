536ca944a290cea3a511d00f640e51e40f9a876a
Kristoffer Ahl <git@77dynamite.com>
2014-03-17 08:35:24 +0100

    - Updated Koshu to 0.6.2.

--- koshu.ps1
+++ koshu.ps1
@@ -1,7 +1,8 @@
 Param(
-	[Parameter(Position=0,Mandatory=1)] [string]$buildFile,
+	[Parameter(Position=0,Mandatory=0)] [string]$buildFile,
 	[Parameter(Position=1,Mandatory=0)] [string]$target,
-	[Parameter(Position=2,Mandatory=0)] [hashtable]$parameters = @{}
+	[Parameter(Position=2,Mandatory=0)] [hashtable]$parameters = @{},
+	[Parameter(Position=3,Mandatory=0)] [switch]$load
 )
 
 # Ensure errors fail the build
@@ -11,13 +12,18 @@ $ErrorActionPreference = 'Stop'
 $nuget = (Get-ChildItem -Path . -Filter NuGet.exe -Recurse | Select-Object -First 1)
 if ($nuget) { $nuget = $nuget.FullName } else { $nuget = "NuGet.exe" }
 try {
-	& $nuget install Koshu -version 0.5.1 -outputdirectory ".\packages"
+	& $nuget install Koshu -version 0.6.2 -outputdirectory ".\Packages"
 } catch [System.Management.Automation.CommandNotFoundException] {
 	throw 'Could not find NuGet.exe and it does not seem to be in your path! Aborting build.'
 }
 
-# Initialize koshu
-.\packages\Koshu.0.5.1\tools\init.ps1
+$initParameters = $parameters.clone()
+if (-not $load) { $initParameters.nologo = $true }
 
-# Trigger koshu
-Koshu-Build $buildFile $target $parameters
+# Initialize koshu
+.\Packages\Koshu.0.6.2\tools\init.ps1 -parameters $initParameters
+
+if (-not $load) {
+	# Trigger koshu
+	Koshu-Build $buildFile $target $parameters
+}
