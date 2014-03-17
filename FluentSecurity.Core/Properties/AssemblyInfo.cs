c1a1bf3ddec3886de0d03acfb3af631fee9c1255
Kristoffer Ahl <git@77dynamite.com>
2014-03-17 09:20:07 +0100

    - Created a project for FluentSecurity.Core.

---
+++ FluentSecurity.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("FluentSecurity.Core")]
+[assembly: InternalsVisibleTo("FluentSecurity.Specification")]
+[assembly: InternalsVisibleTo("FluentSecurity.TestHelper")]
+[assembly: InternalsVisibleTo("FluentSecurity.TestHelper.Specification")]
