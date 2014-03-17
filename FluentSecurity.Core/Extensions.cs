11ad1746c64967cac8f209841f47391f8c920ea3
Kristoffer Ahl <git@77dynamite.com>
2014-03-17 12:49:04 +0100

    - Moved all policies not directly tied to the Mvc implemenation to the Core project.

---
+++ FluentSecurity.Core/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FluentSecurity
+{
+	public static class Extensions
+	{
+		/// <summary>
+		/// Ensures we are working with a list of T
+		/// </summary>
+		internal static IList<T> EnsureIsList<T>(this IEnumerable<T> items)
+		{
+			return items == null
+				? new List<T>()
+				: (items as IList<T> ?? items.ToList());
+		}
+	}
+}
