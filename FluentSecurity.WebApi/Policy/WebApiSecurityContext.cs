1b0d89cb31dcbc0ca3bb0fe7c144b39c89b87a5e
Kristoffer Ahl <git@77dynamite.com>
2014-04-22 21:28:15 +0200

    - Added a WebApiSecurityContext to the WebApi project.

---
+++ FluentSecurity.WebApi/Policy/WebApiSecurityContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using FluentSecurity.Policy.Contexts;
+
+namespace FluentSecurity.WebApi.Policy
+{
+	public class WebApiSecurityContext : SecurityContextWrapper
+	{
+		public WebApiSecurityContext(ISecurityContext innerSecurityContext) : base(innerSecurityContext)
+		{
+			RouteValues = Data.RouteValues;
+		}
+
+		public IDictionary<string, object> RouteValues { get; private set; }
+	}
+}
