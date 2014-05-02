d2a157badede0127a5075fbaac8cca04e1e244ce
Kristoffer Ahl <git@77dynamite.com>
2014-05-02 21:41:49 +0200

    - Added extension classes for LambdaExpression and MethodInfo to Core.

---
+++ FluentSecurity.Core/Internals/LambdaExpressionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace FluentSecurity.Core.Internals
+{
+	public static class LambdaExpressionExtensions
+	{
+		public static MethodInfo GetActionMethodInfo(this LambdaExpression actionExpression)
+		{
+			var expression = actionExpression.Body is UnaryExpression
+				? ((UnaryExpression)actionExpression.Body).Operand
+				: actionExpression.Body;
+
+			return ((MethodCallExpression)expression).Method;
+		}
+	}
+}
---
+++ FluentSecurity.Core/Internals/MethodInfoExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace FluentSecurity.Core.Internals
+{
+	public static class MethodInfoExtensions
+	{
+		public static bool IsDeclaredBy<T>(this MethodInfo methodInfo)
+		{
+			var passedType = typeof(T);
+			var declaringType = methodInfo.GetBaseDefinition().DeclaringType;
+			return declaringType != null && declaringType.IsAssignableFrom(passedType);
+		}
+
+		public static bool HasAttribute<TAttribute>(this MethodInfo methodInfo) where TAttribute : Attribute
+		{
+			return methodInfo.GetCustomAttributes(typeof(TAttribute), false).Any();
+		}
+	}
+}
