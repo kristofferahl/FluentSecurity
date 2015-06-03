0dd5569ce1afb530b8899b7337453a425c7bc30b
Kristoffer Ahl <git@77dynamite.com>
2015-06-03 11:17:06 +0200

    - Fixed the failing ObjectCache spec and added addition spec for the new Set method.

--- FluentSecurity.Specification/ServiceLocation/LifeCycles/ObjectCacheSpec.cs
+++ FluentSecurity.Specification/ServiceLocation/LifeCycles/ObjectCacheSpec.cs
@@ -24,7 +24,7 @@ namespace FluentSecurity.Specification.ServiceLocation.LifeCycles
 		}
 
 		[Test]
-		public void Should_throw_when_key_is_not_unique()
+		public void Should_not_throw_when_key_is_not_unique()
 		{
 			// Arrange
 			var key = Guid.NewGuid();
@@ -32,10 +32,8 @@ namespace FluentSecurity.Specification.ServiceLocation.LifeCycles
 			var obj1 = new object();
 			var obj2 = new object();
 
-			objectCache.Set(key, obj1);
-
 			// Act & assert
-			Assert.Throws<ArgumentException>(() => objectCache.Set(key, obj2));
+			Assert.DoesNotThrow(() => objectCache.Set(key, obj1));
 		}
 
 		[Test]
@@ -49,6 +47,60 @@ namespace FluentSecurity.Specification.ServiceLocation.LifeCycles
 
 			var objectCache = new ObjectCache();
 			objectCache.Set(key, disposable.Object);
+
+			// Act
+			objectCache.Clear();
+
+			// Assert
+			Assert.That(objectCache.Count, Is.EqualTo(0));
+			disposable.Verify(x => x.Dispose(), Times.Exactly(1));
+		}
+	}
+
+	[TestFixture]
+	[Category("ObjectCacheSpec")]
+	public class When_adding_an_object_in_the_ObjectCache
+	{
+		[Test]
+		public void Should_not_add_to_cache_when_instance_is_null()
+		{
+			// Arrange
+			var key = Guid.NewGuid();
+			var objectCache = new ObjectCache();
+
+			// Act
+			objectCache.Add(key, null);
+
+			// Assert
+			Assert.That(objectCache.Count, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void Should_throw_when_key_is_not_unique_using()
+		{
+			// Arrange
+			var key = Guid.NewGuid();
+			var objectCache = new ObjectCache();
+			var obj1 = new object();
+			var obj2 = new object();
+
+			objectCache.Add(key, obj1);
+
+			// Act & assert
+			Assert.Throws<ArgumentException>(() => objectCache.Add(key, obj2));
+		}
+
+		[Test]
+		public void Should_dispose_IDisposable_instances_when_cleared()
+		{
+			// Arrange
+			var key = Guid.NewGuid();
+
+			var disposable = new Mock<IDisposable>();
+			disposable.Setup(x => x.Dispose());
+
+			var objectCache = new ObjectCache();
+			objectCache.Add(key, disposable.Object);
 
 			// Act
 			objectCache.Clear();
